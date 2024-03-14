935e2695fc37bf1d95704f9e906d3161dc76ee1b
Khaled Mamdouh <khalidmamdou7@gmail.com>
2024-03-15 00:06:13 +0200

    Implement include_one command
    include_one command is like inlcude command but could load multiple config files in one command.
    Instead of invoking include multiple times, u could just invoke include_one <config_file_1> <config_file_2> ...

--- sway/commands/include.c
+++ sway/commands/include.c
@@ -12,3 +12,18 @@ struct cmd_results *cmd_include(int argc, char **argv) {
 
 	return cmd_results_new(CMD_SUCCESS, NULL);
 }
+
+
+struct cmd_results *cmd_include_one(int argc, char **argv) {
+	struct cmd_results *error = NULL;
+	if ((error = checkarg(argc, "include_one", EXPECTED_AT_LEAST, 1))) {
+		return error;
+	}
+
+	for (int i = 0; i < argc; i++) {
+		load_include_configs(argv[i], config, &config->swaynag_config_errors);
+	}
+
+	return cmd_results_new(CMD_SUCCESS, NULL);
+
+}
